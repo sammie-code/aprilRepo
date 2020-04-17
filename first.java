b708891e3186fd1facbd302a7eadf5bf7fb675a7
sammie-code <63288550+sammie-code@users.noreply.github.com>
2020-04-17 09:04:16 +0530

    and show time

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 class first{  
     public static void main(String args[]){  
-     System.out.println("Hello Java this is my fifth commit");  
+     System.out.println("Hello Java this is my sixth commit");  
     }  
 } 
